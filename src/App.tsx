7f5844d404f98e95715afd1d43584bedb746778f
Joseph S <55706784+jwrsandilands@users.noreply.github.com>
2023-05-26 10:10:51 +0100

    Finish Following Todo tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import InputField from './components/InputField';
+import { Todo } from './components/model';
+import TodoList from './components/TodoList';
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if(todo){
+      setTodos([...todos, {id: Date.now(), todo, isDone:false}]);
+      setTodo("")
+    }
+
+  };
+
+  console.log(todo);
+
   return (
     <div className="App">
       <span className = "heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      <TodoList todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
